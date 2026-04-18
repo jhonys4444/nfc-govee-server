9556a85d8c0bfd6303be60acae8c495b454ace5c
Camilo <jhonys094444@gmail.com>
2026-04-17 19:51:04 -0500

    Agregar seguridad 3

--- nfc_server.py
+++ nfc_server.py
@@ -1,11 +1,10 @@
-from flask import Flask, request
+from flask import Flask
 import requests
 import os
 
 app = Flask(__name__)
 
 API_KEY = os.environ.get("GOVEE_API_KEY")
-
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 headers = {
@@ -20,49 +19,41 @@ devices = [
     "8C:57:98:17:3C:6F:FD:E4"
 ]
 
-def set_light(device, state):
+def set_light(state):
     url = "https://openapi.api.govee.com/router/api/v1/device/control"
 
-    payload = {
-        "requestId": "nfc-render",
-        "payload": {
-            "sku": "H6008",
-            "device": device,
-            "capability": {
-                "type": "devices.capabilities.on_off",
-                "instance": "powerSwitch",
-                "value": 1 if state else 0
+    for device in devices:
+        payload = {
+            "requestId": "nfc",
+            "payload": {
+                "sku": "H6008",
+                "device": device,
+                "capability": {
+                    "type": "devices.capabilities.on_off",
+                    "instance": "powerSwitch",
+                    "value": 1 if state else 0
+                }
             }
         }
-    }
 
-    requests.post(url, json=payload, headers=headers)
-
-
-def check_key():
-    key = request.args.get("key", "")
-    return key and key == SECRET_KEY
+        requests.post(url, json=payload, headers=headers)
 
 
-@app.route("/on")
-def on():
-    if not check_key():
+@app.route("/on/<token>")
+def on(token):
+    if token != SECRET_KEY:
         return "unauthorized", 403
 
-    for d in devices:
-        set_light(d, 1)
-
+    set_light(1)
     return "ON OK"
 
 
-@app.route("/off")
-def off():
-    if not check_key():
+@app.route("/off/<token>")
+def off(token):
+    if token != SECRET_KEY:
         return "unauthorized", 403
 
-    for d in devices:
-        set_light(d, 0)
-
+    set_light(0)
     return "OFF OK"
 
 
